18130b85b463405ffcaa22146ca40f6a3f335aa1
BekBrace <bekdevelopment@gmail.com>
2022-06-28 06:22:20 -0500

    added promise and .then

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
-import { useRef } from 'react'; 
+import { useRef , useEffect} from 'react'; 
+import * as faceapi from "face-api.js";
 
 function App() {
   const imageRef = useRef()
   const canvasRef = useRef()
+  useEffect(() => {
+    const Models = () => {
+      Promise.all([
+        faceapi.nets.tinyFaceDetector.loadFromUri("/models"),
+        faceapi.nets.faceLandmark68Net.loadFromUri("/models"),
+        faceapi.nets.faceRecognitionNet.loadFromUri("/models"),
+        faceapi.nets.faceExpressionNet.loadFromUri("/models"),
+      ])
+        .then (console.log("promised that i will finish"))
+        .catch((e)=>console.log(e))
+    };
+    //to prevent finishing loading the models before loading the image, i'll write a condition:
+    imageRef.current && Models();
+  });
+
   return (
     <div className="App">
      <img 
